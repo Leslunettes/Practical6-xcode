fa8acaed02c95b7625ac5abaf9bec99192c1ddad
LesLunettes <adrienpourtier@orange.fr>
2017-10-30 18:54:51 +0000

    Initial Commit

---
+++ Practical6-xcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Practical6-xcode
+//
+//  Created by Adrien Pourtier on 30/10/2017.
+//  Copyright © 2017 Adrien Pourtier. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
